459e5253b7be86af92e7724e848b882ca04ac53f
trolleador <eliu_vfa@hotmail.com>
2022-11-11 04:24:41 -0700

    a

---
+++ classes.py
@@ -0,0 +1,12 @@
+
+
+# los nombres de las clases deben de empezar con mayúscula.
+class Player:
+
+    #función iniciadora de la clase
+    def __init__(self, position_x, position_y):
+        self.x = position_x
+        self.y = position_y
+
+yo = Player(1,1)
+
